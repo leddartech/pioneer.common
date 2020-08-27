679edac44c91194d8ea5843c85f678e392fa397f
Francis Tremblay <user361@leddartech.local>
2020-08-27 15:56:23 -0400

    ci test 3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="leddartech-common", # Replace with your own username
+    name="leddartech_common", # Replace with your own username
     version="0.0.1",
     author="Leddartech",
     description="Leddartech's common utilities",
