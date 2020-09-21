9c688f458263838f1d117fb6f05309bc80279415
Francis Tremblay <francis.tremblay@leddartech.com>
2020-09-21 10:01:01 -0400

    feat: Updates package to namespace package

--- setup.py
+++ setup.py
@@ -14,6 +14,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    include_package_data=True
+    python_requires='>=3.6'
 )
