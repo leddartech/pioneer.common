d35d6459608284bebe66691ec441c2945ba45891
Francis Tremblay <francis.tremblay@leddartech.com>
2020-12-17 14:16:02 -0500

    feat: updates 1.0.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_reqs = parse_requirements('requirements.txt')
 
 setuptools.setup(
     name="pioneer_common", # Replace with your own username
-    version="0.6.0",
+    version="1.0.0",
     author="Leddartech",
     description="Pioneer team common utilities",
     packages=[
