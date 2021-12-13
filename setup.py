64a1cb6810dc9b33d5b8bb77cf31cdaf4636812d
Xavier Ouimet <xavier.ouimet@leddartech.com>
2021-12-13 15:16:50 -0500

    feat: version for 1.2.0 release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_reqs = parse_requirements('requirements.txt')
 
 setuptools.setup(
     name="pioneer_common", # Replace with your own username
-    version="1.1.0",
+    version="1.2.0",
     author="Leddartech",
     description="Pioneer team common utilities",
     packages=[
